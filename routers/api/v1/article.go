f03f3314f33d7c2fae74b2a87c966f62149e5768
Matt Chang <tingyugenius@gmai.com>
2019-06-25 16:51:22 +0800

    add article router

---
+++ routers/api/v1/article.go
@@ -0,0 +1,25 @@
+package v1
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func GetArticle(c *gin.Context) {
+
+}
+
+func GetArticles(c *gin.Context) {
+
+}
+
+func AddArticle(c *gin.Context) {
+
+}
+
+func EditArticle(c *gin.Context) {
+
+}
+
+func DeleteAritcle(c *gin.Context) {
+
+}
