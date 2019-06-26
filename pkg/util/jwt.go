dca5f2c37cb387f1a50d104f027e87d4c3cced5a
Matt Chang <tingyugenius@gmai.com>
2019-06-26 09:12:14 +0800

    add util jwt.go

---
+++ pkg/util/jwt.go
@@ -0,0 +1,52 @@
+package util
+
+import (
+	"log"
+	"time"
+
+	jwt "github.com/dgrijalva/jwt-go"
+
+	"goweb/pkg/setting"
+)
+
+var jwtSecret = []byte(setting.JwtSecret)
+
+type Claims struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+	jwt.StandardClaims
+}
+
+func GenerateToken(username, password string) (string, error) {
+	nowTime := time.Now()
+	expiredTime := nowTime.Add(3 * time.Hour)
+
+	claims := Claims{
+		usename,
+		password,
+		jwt.StandardClaims{
+			ExpiresAt: expireTime.Unix(),
+			Issuer:    "goweb",
+		},
+	}
+
+	tokenClaims := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	token, err := tokenClaims.SignedString(jwtSecret)
+
+	log.Println(token)
+	return token, err
+}
+
+func ParseToken(token string) (*Claims, error) {
+	tokenClaims, err := jwt.ParseWithClaims(token, &Claims{}, func(token *jwt.Token) (interface{}, error) {
+		return jwtSecret, nil
+	})
+
+	if tokenClaims != nil {
+		if claims, ok := tokenClaims.Claims.(*Claims); ok && tokenClaims.Valid {
+			return claims, nil
+		}
+	}
+
+	return nil, err
+}
