1fd041431caf824f5ea677beef873bb8607479bc
Matt Chang <tingyugenius@gmai.com>
2019-06-26 19:13:20 +0800

    use net/http for hot-restart

--- main.go
+++ main.go
@@ -1,8 +1,15 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	// "github.com/fvbock/endless"
 
 	"goweb/pkg/setting"
 	"goweb/routers"
@@ -19,5 +26,23 @@ func main() {
 		MaxHeaderBytes: 1 << 20,
 	}
 
-	s.ListenAndServe()
+	go func() {
+		if err := s.ListenAndServe(); err != nil {
+			log.Printf("Listen: %s\n", err)
+		}
+	}()
+
+	quit := make(chan os.Signal)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+
+	log.Println("Shutdown Server ...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := s.Shutdown(ctx); err != nil {
+		log.Fatal("Server Shutdown:", err)
+	}
+
+	log.Println("Server exiting")
 }
